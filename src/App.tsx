74c6c28dd13aab46cddd055ea385ca79ffcfd9c5
hhs0001 <heitorstein@gmail.com>
2026-01-12 12:24:18 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import { Button } from "@heroui/react";
+
+function App() {
+  return (
+    <main className="container">
+      <Button>My Button</Button>
+    </main>
+  );
+}
+
+export default App;
